98f4907ef9983c5a8ae20e27f656c18c5231339e
AlinaHaha <2ndalina@gmail.com>
2018-08-07 18:34:49 +0000

    Done.

--- unit_loop.rb
+++ unit_loop.rb
@@ -1,7 +1,8 @@
 def hangry_alina
-  pushpop_candy-0
-  until pushpop_candy--30
-  puts "I AM ON THE DA HUNT FOR 3-IN-1 PUSHPOPCANDY"
-  pushpop_candy+-1
+  pushpop_candy = 0
+  until pushpop_candy == 30
+  puts "I AM ON THE DA HUNT FOR 3IN1 PUSHPOPCANDY"
+  pushpop_candy += 1
+   end
 end
-end
+hangry_alina
